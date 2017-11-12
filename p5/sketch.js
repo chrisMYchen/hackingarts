151e3e9baf98fb4fc4e95817f8f51c0e7cfab746
Xuefei Yang <xyang04@risd.edu>
2017-11-12 00:25:25 -0500

    play around with ellipses and rects

--- p5/sketch.js
+++ p5/sketch.js
@@ -1,9 +1,18 @@
 function setup() {
   createCanvas(windowWidth, windowHeight)
+  blendMode(BURN)
 }
 
 function draw() {
-  if(accelerationX > 10){
-  rect(random(300), random(670),accelerationX, accelerationY)
+  background(255,5)
+ noStroke()
+  if (accelerationX > 1 && accelerationX < 3) {
+    fill(255,87,159)
+    rect(random(windowWidth), random(windowHeight), accelerationX * 10, accelerationY * 10)
+  } else if (accelerationX >= 3 && accelerationX <10) {
+    noStroke()
+    fill(155,77,253)
+    ellipse(random(windowWidth), random(windowHeight), (accelerationX*10)^2, (accelerationX*10)^2)
+    
   }
 }
