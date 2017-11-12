01d858135bb26ac860c8ad2c250a4b51f1cfbb8e
chrisMYchen <christopher_chen@brown.edu>
2017-11-11 23:10:35 -0500

    Added kat's page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var path    = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/kat',function(req,res){
+  res.sendFile(path.join(__dirname+'/kat.html'));
 });
 
 module.exports = router;
